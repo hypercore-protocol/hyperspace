e83d7a4544b8fe3ef19fdb94c837ced7b9c2f899
Andrew Osheroff <andrewosh@gmail.com>
2020-06-10 11:28:36 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const Corestore = require('corestore')
+const Networker = require('corestore-swarm-networking')
+const { NanoresourcePromise: Nanoresource } = require('nanoresource-promise/emitter')
+const rpc = require('arpeecee')
+
+class Hyperspace extends Nanoresource {
+  constructor (opts = {}) {
+    super()
+  }
+}
+
+module.exports = Hyperspace
